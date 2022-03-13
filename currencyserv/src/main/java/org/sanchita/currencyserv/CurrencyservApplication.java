aa8a8f2c6e043981c5d9a0a0bd58abf2fcba863e
sanchbis <sanchbis@in.ibm.com>
2022-03-14 00:31:48 +0530

    Added currency service

---
+++ currencyserv/src/main/java/org/sanchita/currencyserv/CurrencyservApplication.java
@@ -0,0 +1,13 @@
+package org.sanchita.currencyserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyservApplication.class, args);
+	}
+
+}
---
+++ currencyserv/src/test/java/org/sanchita/currencyserv/CurrencyservApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sanchita.currencyserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
