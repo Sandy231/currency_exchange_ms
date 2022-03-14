fe350f32c9ecd8fa1d6c69fdb568174a7fe6cd5f
Souradeep Biswas <sourabiswas@paypal.com>
2022-03-14 14:21:05 +0530

    Added Load Balanced Rest Template

--- currencyserv/src/main/java/org/sanchita/currencyserv/CurrencyservApplication.java
+++ currencyserv/src/main/java/org/sanchita/currencyserv/CurrencyservApplication.java
@@ -2,9 +2,18 @@ package org.sanchita.currencyserv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CurrencyservApplication {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CurrencyservApplication.class, args);
