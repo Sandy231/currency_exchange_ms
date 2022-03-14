69cc62c1cbc5cec766e0c0859c3de35b9fb5c311
Souradeep Biswas <sourabiswas@paypal.com>
2022-03-14 14:22:09 +0530

    Added Convert Currency to CurrencyController.java

---
+++ currencyserv/src/main/java/org/sanchita/currencyserv/controller/CurrencyController.java
@@ -0,0 +1,37 @@
+package org.sanchita.currencyserv.controller;
+
+import org.sanchita.currencyserv.service.CurrencyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+public class CurrencyController {
+
+    @Autowired
+    CurrencyService currencyService;
+
+    @GetMapping("/currency")
+    public ResponseEntity<Map<String,String>> convertCurrency(@RequestParam String countryCode, @RequestParam double amount){
+
+        Optional<Double> response = currencyService.convertCurrency(countryCode, amount);
+        if(response.isPresent()){
+            Map<String,String> map = new HashMap<>();
+            map.put("Country Code",countryCode);
+            map.put("Base Amount",amount+"");
+            map.put("Converted Amount",response.get().toString());
+            return ResponseEntity.ok(map);
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,String.format("Conversion Factor with Country Code %s not present. Currency Conversion Failed!",countryCode));
+        }
+
+    }
+}
